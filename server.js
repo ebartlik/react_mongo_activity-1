deca0bf117586822e5441c4d619eb604e2e4f036
ebartlik <ebartlik@gmail.com>
2018-10-06 11:31:44 -0400

    commit

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+const Example = require("./exampleModel.js");
+
+//remember that this is your connection string.
+//we will change this later
+mongoose.connect("mongodb://localhost/testmern", { useNewUrlParser: true });
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,12 +18,43 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.get("/michigan", (req, res) => {
+  res.json({
+    test: "uh Kristine"
+  });
+});
+
+app.get("/testdb", (req, res) => {
+  // Create an object containing dummy data to save to the database
+var data = {
+  array: ["item1", "item2", "item3"],
+  boolean: false,
+  string:
+    "\"Don't worry if it doesn't work right. If everything did, you'd be out of a job\" - Mosher's Law of Software Engineering",
+  number: 42
+};
+
+// Save a new Example using the data object
+Example.create(data)
+  .then(function(dbExample) {
+    // If saved successfully, print the new Example document to the console
+    console.log(dbExample);
+    res.json(dbExample);
+  })
+  .catch(function(err) {
+    // If an error occurs, log the error message
+    console.log(err.message);
+  });
+
+});
 
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
